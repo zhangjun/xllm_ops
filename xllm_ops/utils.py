1bf536f583d2898bc1db711219ca4cef2368d6b4
zhangjun <ewalker@live.cn>
2025-04-14 11:01:39 +0800

    update

--- xllm_ops/utils.py
+++ xllm_ops/utils.py
@@ -1,6 +1,34 @@
 from typing import Dict, Tuple
+import functools
 
 import torch
+
+# https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#features-and-technical-specifications-technical-specifications-per-compute-capability
+WARPS_PER_SM = {
+    (8, 0): 64,
+    (8, 6): 48,
+    (8, 7): 48,
+    (8, 9): 48,
+    (9, 0): 64,
+    (10, 0): 64,
+    (10, 1): 48,
+    (12, 0): 48,
+}
+
+
+@functools.lru_cache
+def get_device_props(device=None):
+    if device is None:
+        device = torch.cuda.current_device()
+
+    props = torch.cuda.get_device_properties(device)
+
+    warps_per_sm = WARPS_PER_SM.get((props.major, props.minor), 32)
+    out = dict(
+        multi_processor_count=props.multi_processor_count,
+        warps_per_sm=warps_per_sm,
+    )
+    return out
 
 def get_cuda_stream() -> int:
     return torch.cuda.current_stream().cuda_stream
